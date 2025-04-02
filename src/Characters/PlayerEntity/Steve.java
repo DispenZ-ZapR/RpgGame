0b14d642c82361faaa8169d24cad608f19d487fd
emir <aitbemr@gmail.com>
2025-04-03 00:38:41 +0600

    -added new class "Steve"

---
+++ src/Characters/PlayerEntity/Steve.java
@@ -0,0 +1,53 @@
+package Characters.PlayerEntity;
+
+import java.util.Random;
+
+public class Steve extends EntityStatistic{
+    @Override
+    public int attack() {
+        if (stamina >= 10) {
+        Random r = new Random();
+        int dam = damage + r.nextInt(5);
+        return dam;
+        }else {
+            System.out.println("Не достаточно выносливости");
+            return 0;
+        }
+    }
+    boolean defense = false;
+    @Override
+    public void skill() {
+        if (stamina > 25) {
+            System.out.println("Навык: Поглощение-пошлощает 20% урона");
+            stamina -= 25;
+        }else {
+
+            System.out.println("Не достаточно выносливости");
+        }
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        if (!defense) {
+            heatlh -= (20/ 100) * damage;
+        }else {
+            heatlh -= damage;
+        }
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Steve");
+        System.out.println("Health: "+ heatlh + "Damage: "+ damage + "Stamina: "+ stamina);
+    }
+
+    @Override
+    public void Recovery() {
+        stamina += 25;
+    }
+
+    public Steve(int heatlh, int damage, int stamina, int id) {
+        super(100,45,75,PlayerType.STEVE,4);
+
+    }
+}
