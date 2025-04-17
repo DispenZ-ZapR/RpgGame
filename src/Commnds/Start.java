0d6811eccee95f7efabdcf24811b566a03e5c3fc
emir <aitbemr@gmail.com>
2025-04-17 22:32:59 +0600

    Added methods: checker and villiamChecker for checking the state of the player and the opponent, attack for processing combat actions, recovery and skill for recovering and applying skills. Implemented logic of random opponent selection in randomvilliam method.

--- src/Commnds/Start.java
+++ src/Commnds/Start.java
@@ -2,7 +2,7 @@ package Commnds;
 
 import Characters.PlayerEntity.*;
 import Characters.VilliamEntity.*;
-import UserData.User;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,66 +10,58 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Start implements Command {
-    Scanner sc = new Scanner(System.in);
-    PlayerType playerType;
-    Random rand = new Random();
-    EntityStatistic player = null;
-    VilliamStatistic villiam = null;
-    List<VilliamStatistic> villiamStorage = new ArrayList<>();
+    PlayerType type;
+    EntityStatistic entity;
+    boolean pLayerLivechecker = true;
+    boolean villiamLivechecker = true;
+    VilliamStatistic villiam;
 
-    @Override
-    public String toString() {
-        return "Start - начать игру";
-    }
+    Random rand = new Random();
+
+    Scanner sc = new Scanner(System.in);
 
     @Override
     public void execute() {
-        System.out.println("Выберите класс персонажа");
-        System.out.println("1)"+PlayerType.WARRIOR.toString() + " 2)" + PlayerType.ALEKSANDR + " 3)" + PlayerType.WIZARD);
-        int choice = sc.nextInt();
-        villiamStorage.add(new Barbarian(80,80,60,1));
-        villiamStorage.add(new Goblin(40,30,50, TypeVilliam.GOBLIN,1));
-        villiamStorage.add(new DeadWarrior(60,70,70, TypeVilliam.DEAD_WARRIOR,3));
-        VilliamStatistic randomVilliam = villiamStorage.get(rand.nextInt(villiamStorage.size()));
-        switch (choice) {
-            case 1:
-                player = new Warrior(60,60, 60,1);
-                break;
-            case 2:
-                player = new Aleksandr(100,100,100,2);
-                break;
-            case 3:
-                player = new Wizard(40,70,60,3);
-                break;
-        }
-        while(player.isAlive() && randomVilliam.isAlive()) {
-            System.out.println(randomVilliam.display());
-            System.out.println("1)Атака \n" +
-                    "2)Восстановление \n" +
-                    "3)Навык ");
-            player.display();
-            int choice2 = sc.nextInt();
-            switch (choice2) {
-                case 1:
-                    randomVilliam.takeDamage(player.attack());
-                    player.takeDamage(randomVilliam.attack());
-                    player.isAlive();
-                    randomVilliam.isAlive();
-                    break;
-                case 2:
-                    player.Recovery();
-                    player.takeDamage(randomVilliam.attack());
-                    player.isAlive();
-                    randomVilliam.isAlive();
-                    break;
-                case 3:
-                    player.skill();
-                    player.takeDamage(randomVilliam.attack());
-                    player.isAlive();
-                    randomVilliam.isAlive();
-                    break;
-            }
+        
+    }
 
+    public void checker(){
+        if (entity.isAlive()){
+           pLayerLivechecker = true;
+        }else {
+            System.out.println("Вы пройграли!");
+            pLayerLivechecker = false;
         }
+    }
+    public void villiamChecker(){
+        if (entity.isAlive()){
+            villiamLivechecker = true;
+        }else {
+            System.out.println("Монстр побежден!");
+            villiamLivechecker = false;
+        }
+
+    }
+    public void attack(){
+        villiam.takeDamage(entity.getDamage());
+        entity.takeDamage(villiam.attack());
+
+    }
+    public void recovery(){
+        entity.Recovery();
+        entity.takeDamage(randomvilliam().attack());
+    }
+    public void skill(){
+        entity.skill();
+        entity.takeDamage(randomvilliam().attack());
+    }
+
+    public VilliamStatistic randomvilliam(){
+        List<VilliamStatistic> mobs = new ArrayList<VilliamStatistic>();
+        mobs.add(new Barbarian());
+        mobs.add(new DeadWarrior());
+        mobs.add(new Goblin());
+        villiam = mobs.get(rand.nextInt(mobs.size()));
+        return villiam;
     }
 }
