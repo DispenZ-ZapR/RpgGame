9b06c39382c0f17d334eadc523746ac9703cf678
emir <aitbemr@gmail.com>
2025-04-24 02:19:01 +0600

    -added numbers to the enum descriptions

--- src/Characters/PlayerEntity/PlayerType.java
+++ src/Characters/PlayerEntity/PlayerType.java
@@ -1,13 +1,19 @@
 package Characters.PlayerEntity;
 
 public enum PlayerType {
-    WARRIOR("Рыцарь"),
-    ALEKSANDR("Александр"),
-    WIZARD("Маг"),
-    STEVE("Стив из Майнкрафта");
+    WARRIOR("1)Рыцарь"),
+    ALEKSANDR("2)Александр"),
+    WIZARD("3)Маг"),
+    STEVE("4)Стив из Майнкрафта");
     String description;
 
     PlayerType(String description) {
         this.description = description;
     }
+    public static void getDescription() {
+        for (PlayerType type : PlayerType.values()) {
+            System.out.println(type.description);
+        }
+    }
 }
+
